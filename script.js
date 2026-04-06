8a1a50b91f0d8e959060588c306697d71228853a
vivi051122 <svt17.bsuga@gmail.com>
2026-04-06 16:19:47 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -22,14 +22,15 @@ window.addEventListener('load',function(){
 console.log(pullDownButton)
 
     pullDownButton.addEventListener('mouseover',function(){
-        pullDownButton.setAttribute("style","background-color:blue;")
+        this.setAttribute("style","background-color:blue;")
+        console.log("乗ったら青色")
 })
     pullDownButton.addEventListener('mouseout',function(){
-        pullDownButton.removeAttribute("style","background-color:red;")
+        this.removeAttribute("style","background-color:red;")
     })
 
     pullDownButton.addEventListener('click',function(){
-        pullDownButton.setAttribute("style","background-color:green;")
+        this.setAttribute("style","background-color:green;")
     })
 })
 
@@ -49,4 +50,17 @@ window.addEventListener('click',function(){
 
     const pullDownButton = document.getElementById("lists")
     console.log("クリック")
+})
+
+const pullDownButton = document.getElementById("lists")
+const pullDownParents = document.getElementById("pull-down")
+
+pullDownButton.addEventListener('click',function(){
+    if(pullDownParents.getAttribute("style")=="display:block;"){
+        pullDownParents.removeAttribute("style","display:block;")
+        console.log("非表示")
+    } else{
+        pullDownParents.setAttribute("style","display:block;")
+        console.log("表示")
+    }
 })
