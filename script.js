9894e76a03d89b5bb262e6f1ccd181390be8d52b
vivi051122 <svt17.bsuga@gmail.com>
2026-04-06 13:04:33 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -20,6 +20,17 @@ window.addEventListener('load',function(){
 
     const pullDownButton = document.getElementById("lists")
 console.log(pullDownButton)
+
+    pullDownButton.addEventListener('mouseover',function(){
+        pullDownButton.setAttribute("style","background-color:blue;")
+})
+    pullDownButton.addEventListener('mouseout',function(){
+        pullDownButton.removeAttribute("style","background-color:red;")
+    })
+
+    pullDownButton.addEventListener('click',function(){
+        pullDownButton.setAttribute("style","background-color:green;")
+    })
 })
 
 window.addEventListener('mouseover',function(){
